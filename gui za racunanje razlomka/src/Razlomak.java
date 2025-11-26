4c32dbf0ad248c00e8ba8cc1aff23c3dbeed00f3
Vasilije Scepanovic <scepanovic701@gmail.com>
2025-11-26 13:18:25 +0100

    Add files via upload

---
+++ gui za racunanje razlomka/src/Razlomak.java
@@ -0,0 +1,103 @@
+
+public class Razlomak {
+
+	private int brojilac;
+	private int imenilac;
+	
+	
+	
+	public Razlomak () {
+		this(0,1);
+	}
+	
+	public Razlomak(int brojilac, int imenilac) {
+		super();
+		this.setBrojilac( brojilac);
+		this.setImenilac (imenilac);
+		this.skrati();
+	}
+	
+	
+	
+	
+	public int getBrojilac() {
+		return brojilac;
+	}
+	public void setBrojilac(int brojilac) {
+		this.brojilac = brojilac;
+		this.skrati();
+	}
+	public int getImenilac() {
+		return imenilac;
+	}
+	public void setImenilac(int imenilac) {
+		this.imenilac = imenilac;
+		this.skrati();
+	}
+	
+	
+	
+	@Override
+	public String toString() {
+		
+		if ((this.getBrojilac() < 0 && this.getImenilac() > 0) || (this.getBrojilac() > 0  && this.getImenilac() < 0)) {
+			return String.format("-%d/%d", Math.abs(this.getBrojilac()), Math.abs(this.getImenilac()));
+			
+		}
+		else
+			return String.format("%d/%d" , Math.abs(this.getBrojilac()), Math.abs(this.getImenilac()));
+	
+	}
+	
+	
+	public static Razlomak saberi(Razlomak a, Razlomak b) {
+		
+	Razlomak rez = new Razlomak();
+	rez.setBrojilac(a.getBrojilac() * b.getImenilac() + b.getBrojilac() * a.getImenilac());
+	rez.setImenilac(a.getImenilac() * b.getImenilac());
+	
+	return rez;
+	
+	}
+	
+	
+	public static Razlomak pomnozi(Razlomak a, Razlomak b) {
+		
+		Razlomak rez = new Razlomak();
+		rez.setBrojilac(a.getBrojilac() * b.getBrojilac());
+		rez.setImenilac(a.getImenilac() * b.getImenilac());
+		
+		return rez;
+		
+	}
+	
+	
+	
+	public static Razlomak podeli(Razlomak a, Razlomak b) {
+		
+		Razlomak rez = new Razlomak();
+		rez.setBrojilac(a.getBrojilac() * b.getImenilac());
+		rez.setImenilac(a.getImenilac() * b.getBrojilac());
+		
+		return rez;
+		
+	}
+	
+	public void skrati () {
+		
+		int a = Math.abs(this.getBrojilac());
+		int b = Math.abs(this.getImenilac());
+		
+		for (int i = 2 ; i <= Math.min(a,b); i++) {
+			while(this.getBrojilac() % i == 0 && this.getImenilac() % i == 0) {
+				this.setBrojilac(this.getBrojilac() / i);
+				this.setImenilac(this.getImenilac() / i);
+			}
+		}
+	}
+	
+	
+	
+	
+	
+}
---
+++ gui za racunanje razlomka/src/Test.java
@@ -0,0 +1,17 @@
+
+public class Test {
+
+	public static void main(String[] args) {
+		
+		
+		Razlomak r1 = new Razlomak(-70, -40
+				);
+		
+		
+		System.out.println(r1);
+r1.setBrojilac(14);
+		System.out.println(r1);
+		
+	}
+
+}
